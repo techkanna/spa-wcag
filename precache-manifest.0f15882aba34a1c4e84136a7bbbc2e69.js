5d517e83a3e42c1533535a4b33fd0d861ef7f006
Senthilkannan <51955692+techkanna@users.noreply.github.com>
2020-08-25 11:22:58 +0530

    initial commit

---
+++ precache-manifest.0f15882aba34a1c4e84136a7bbbc2e69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "374ee8405d11138868460b75b4c8e00f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1d331b29c884dc036a4",
+    "url": "/static/css/main.d3d38996.chunk.css"
+  },
+  {
+    "revision": "29484a944d6c53c59d28",
+    "url": "/static/js/2.19251333.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.19251333.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1d331b29c884dc036a4",
+    "url": "/static/js/main.732dbc2e.chunk.js"
+  },
+  {
+    "revision": "829d57bc314eb6fdeb88",
+    "url": "/static/js/runtime-main.15d75963.js"
+  }
+]);
