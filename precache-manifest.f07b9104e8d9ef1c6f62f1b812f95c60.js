8ac5421a2bae076c96a661d56a074a4366fcd502
Senthilkannan <51955692+techkanna@users.noreply.github.com>
2020-08-25 12:04:02 +0530

    fixed ralative path issue on build

---
+++ precache-manifest.f07b9104e8d9ef1c6f62f1b812f95c60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ccfb1359a101ccd12a3b1f8fc58798b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d1d331b29c884dc036a4",
+    "url": "./static/css/main.d3d38996.chunk.css"
+  },
+  {
+    "revision": "29484a944d6c53c59d28",
+    "url": "./static/js/2.19251333.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.19251333.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1d331b29c884dc036a4",
+    "url": "./static/js/main.732dbc2e.chunk.js"
+  },
+  {
+    "revision": "369231d8fee1f896eb3c",
+    "url": "./static/js/runtime-main.f3eb9d69.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0f15882aba34a1c4e84136a7bbbc2e69.js"
+  "./precache-manifest.f07b9104e8d9ef1c6f62f1b812f95c60.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
